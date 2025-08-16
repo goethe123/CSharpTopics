42848ab7b1f95832c2872d86d5ee7d6872102b23
goethe123 <goethe123123@gmail.com>
2025-08-16 13:14:06 -0600

    EnumLesson

---
+++ Enums/Enum1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enum1
+{
+    public class Program
+    {
+        enum DiasDeLaSemana { lunes = 1, martes, miercoles, jueves, viernes, sabado, domingo };
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Dame un número del 1 al 7:");
+            int dia = Convert.ToInt32(Console.ReadLine());
+
+            if (dia >= 1 && dia <= 7)
+            {
+                DiasDeLaSemana diaEnum = (DiasDeLaSemana)dia;
+                Console.WriteLine($"El día es: {diaEnum}");
+            }
+            else
+            {
+                Console.WriteLine("Número inválido, debe estar entre 1 y 7.");
+            }
+        }
+    }
+}
